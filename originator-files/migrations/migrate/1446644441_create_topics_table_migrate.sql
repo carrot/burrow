b0b99fae561d8c662a82619cb441c6130a8c6f1e
BrandonRomano <BrandonRRomano@gmail.com>
2015-11-04 10:42:44 -0500

    Adding updated_at trigger function

--- originator-files/migrations/migrate/1446644441_create_topics_table_migrate.sql
+++ originator-files/migrations/migrate/1446644441_create_topics_table_migrate.sql
@@ -1,4 +1,12 @@
 CREATE TABLE topics(
     id                  serial          PRIMARY KEY,
-    name                varchar(128)    NOT NULL UNIQUE
+    name                varchar(128)    NOT NULL UNIQUE,
+    created_at          timestamptz     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at          timestamptz     NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TRIGGER topics_updated_at
+BEFORE UPDATE
+ON topics
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
--- originator-files/migrations/revert/1446644441_create_topics_table_revert.sql
+++ originator-files/migrations/revert/1446644441_create_topics_table_revert.sql
@@ -1,1 +1,2 @@
+DROP TRIGGER IF EXISTS topics_updated_at ON topics;
 DROP TABLE topics;
